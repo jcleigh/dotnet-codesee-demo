55387e6b65a5421e7655e87e7cd54820affebd65
Jordan Cleigh <cleigh.jordan@gmail.com>
2023-10-09 07:57:19 -0500

    Refactor GetWeatherForecast into component

---
+++ Components/GetWeatherForecast.cs
@@ -0,0 +1,39 @@
+﻿namespace dotnet_codesee_demo.Components
+{
+    public class GetWeatherForecast
+    {
+        public class Request { }
+
+        public class Response
+        {
+            public WeatherForecast[] Forecast { get; set; }
+        }
+
+        public Response Execute(Request request)
+        {
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    (
+                        DateTime.Now.AddDays(index),
+                        Random.Shared.Next(-20, 55),
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    ))
+                .ToArray();
+
+            return new Response
+            {
+                Forecast = forecast
+            };
+        }
+
+        public record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+        {
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}
